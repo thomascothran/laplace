f82e1bc33251aa13e9854d670ca2ae0a5940630c
Thomas Cothran <tmcothran@gmail.com>
2023-05-27 19:35:08 -0400

    Home route

--- src/main/thomascothran/laplace/routes.clj
+++ src/main/thomascothran/laplace/routes.clj
@@ -1,11 +1,13 @@
 (ns thomascothran.laplace.routes
-  (:require [reitit.ring :as ring]))
+  (:require [reitit.ring :as ring]
+            [reitit.coercion :as coercion]
+            [thomascothran.laplace.routes.ui :as ui]))
 
-(def tree
+(defn tree
+  []
   [""
    ["/" (constantly {:status 308 :headers {"Location" "/ui"}})]
-   ["/ui" {:middleware [[:hiccup/body]]}
-    ["" (constantly {:hiccup/body [:p "Hi there you!"]})]]
+   ["/ui" (ui/routes)]
    ["/assets/*" (ring/create-resource-handler)]
    ["/api/v1/*"
     {:get (constantly {:status 200 :body "coming soon"})}]])
--- src/main/thomascothran/laplace/routes/ui/home.clj
+++ src/main/thomascothran/laplace/routes/ui/home.clj
@@ -1,1 +1,18 @@
-(ns thomascothran.laplace.routes.ui.home)
+(ns thomascothran.laplace.routes.ui.home
+  (:require [thomascothran.laplace.ui-components.organism :as organism]
+            [thomascothran.laplace.routes.ui.charts :as charts]))
+
+(defn home-get
+  [_req]
+  {:hiccup/page
+   [:div.container
+    (organism/simple-model-form {:form/action "/ui/charts/simple-throughput-model"
+                                 :htmx/target "#simple-throughput-model-chart"})
+    [:div#simple-throughput-model-chart]]})
+
+(defn routes
+  []
+  ["" {:middleware [[:hiccup/page]]}
+   [""  {:get home-get}]
+   ["/charts"
+    (charts/routes)]])
