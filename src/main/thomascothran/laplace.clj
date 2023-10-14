46df9b4c27bfa72247b9d71d247aa3e5ef584b26
Thomas Cothran <tmcothran@gmail.com>
2023-10-14 19:50:12 -0400

    Start server from main ns and set default exceptoin handler

--- src/main/thomascothran/laplace.clj
+++ src/main/thomascothran/laplace.clj
@@ -1,12 +1,16 @@
 (ns thomascothran.laplace
+  (:require [thomascothran.laplace.server :as server]
+            [clojure.tools.logging :as log])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread ex]
+     (log/error ex "Uncaught exception on" (.getName thread)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (let [port 80]
+    (server/start! {:server/port port})
+    @(promise)))
